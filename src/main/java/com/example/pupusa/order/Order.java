2fbbe41691b8828953d94138e36e9e51dee3d9fe
JCH <jchn0111@naver.com>
2022-06-06 01:30:19 +0900

    결제시나리오 작성중

--- src/main/java/com/example/pupusa/order/Order.java
+++ src/main/java/com/example/pupusa/order/Order.java
@@ -4,7 +4,6 @@ import com.example.pupusa.chatRoom.ChatRoom;
 import com.example.pupusa.store.Menu;
 import com.example.pupusa.user.User;
 import lombok.*;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.*;
 
