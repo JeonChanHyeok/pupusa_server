ee67dd2007d89784a86cbdc3d8b3448ee031e856
JCH <jchn0111@naver.com>
2022-06-06 01:29:24 +0900

    결제시나리오 작성중

---
+++ src/main/java/com/example/pupusa/order/Order.java
@@ -0,0 +1,40 @@
+package com.example.pupusa.order;
+
+import com.example.pupusa.chatRoom.ChatRoom;
+import com.example.pupusa.store.Menu;
+import com.example.pupusa.user.User;
+import lombok.*;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "orderId")
+    private Long orderId;
+    private boolean inEnd;
+
+    @ManyToOne
+    @JoinColumn(name = "menuId")
+    private Menu menu;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "roomId")
+    private ChatRoom chatRoom;
+
+
+
+
+
+}
